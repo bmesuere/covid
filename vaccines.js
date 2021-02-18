d29d9db7e77324f2b6797ac9526d77e1cee80589
Bart Mesuere <Bart.Mesuere@UGent.be>
2021-02-18 12:29:41 +0100

    update vaccine code

--- vaccines.js
+++ vaccines.js
@@ -1,9 +1,8 @@
-import puppeteer from "puppeteer";
 import d3 from "d3";
+import fetch from "node-fetch";
 import fs from "fs/promises";
 
-
-const dashboardUrl = "https://datastudio.google.com/embed/reporting/c14a5cfc-cab7-4812-848c-0369173148ab/page/hOMwB";
+const fileUrl = "https://epistat.sciensano.be/Data/COVID19BE_VACC.json";
 const dataFile = "data/vaccines_cumulative.csv";
 
 const today = d3.timeFormat("%Y-%m-%d")(d3.utcHour.offset(Date.now(), -2));
@@ -15,22 +14,21 @@ vaccineData.push(`"${yesterday}","Belgium",${vaccinesTotal.belgium}`);
 vaccineData.push(`"${yesterday}","Brussels",${vaccinesTotal.brussels}`);
 vaccineData.push(`"${yesterday}","Flanders",${vaccinesTotal.flanders}`);
 vaccineData.push(`"${yesterday}","Wallonia",${vaccinesTotal.wallonia}`);
+vaccineData.push(`"${yesterday}","Ostbelgien",${vaccinesTotal.ostbelgien}`);
 await writeFileContent(vaccineData);
 
 async function getTotalVaccinations() {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(dashboardUrl);
-  await page.waitForTimeout(10000);
-  const belgiumVaccinated = await page.evaluate(() => +document.querySelector('.scorecard-component').textContent.trim().replaceAll(/[.,]/g, ""));
-  const brusselsVaccinated = await page.evaluate(() => +document.querySelector('[title="Brussels"]').parentElement.querySelector(":nth-child(2)").textContent.trim().replaceAll(/[.,]/g, ""));
-  const flandersVaccinated = await page.evaluate(() => +document.querySelector('[title="Flanders"]').parentElement.querySelector(":nth-child(2)").textContent.trim().replaceAll(/[.,]/g, ""));
-  const walloniaVaccinated = await page.evaluate(() => +document.querySelector('[title="Wallonia"]').parentElement.querySelector(":nth-child(2)").textContent.trim().replaceAll(/[.,]/g, ""));
-  await browser.close();
-  return {belgium: belgiumVaccinated, brussels: brusselsVaccinated, flanders: flandersVaccinated, wallonia: walloniaVaccinated};
+  const rawData = await (await fetch(fileUrl)).json();
+  const belgiumVaccinated = d3.sum(rawData, d => d.COUNT);
+  const brusselsVaccinated = d3.sum(rawData.filter(d => d.REGION === "Brussels"), d => d.COUNT);
+  const flandersVaccinated = d3.sum(rawData.filter(d => d.REGION === "Flanders"), d => d.COUNT);
+  const walloniaVaccinated = d3.sum(rawData.filter(d => d.REGION === "Wallonia"), d => d.COUNT);
+  const ostbelgienVaccinated = d3.sum(rawData.filter(d => d.REGION === "Ostbelgien"), d => d.COUNT);
+  return {belgium: belgiumVaccinated, brussels: brusselsVaccinated, flanders: flandersVaccinated, wallonia: walloniaVaccinated, ostbelgien: ostbelgienVaccinated};
 }
 
 async function getFileContent() {
+  console.log(yesterday);
   return (await fs.readFile(dataFile, 'utf8'))
     .split("\n")
     .filter(line => !line.includes(yesterday))
